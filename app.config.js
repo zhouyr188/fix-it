3014b49d27d20743c4ccbab84fd21137e3ec3156
zhouyr188 <zhouyr188@outlook.com>
2026-09-21 21:57:27 +0800

    版本号归位 1.0.0:月底封版倒计时开始

--- app.config.js
+++ app.config.js
@@ -12,7 +12,7 @@ export default {
   expo: {
     name: 'Fix It',
     slug: 'gfj',
-    version: '1.1.0',
+    version: '1.0.0',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'light',
